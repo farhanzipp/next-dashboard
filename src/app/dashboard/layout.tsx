93afa2ed4e44ed53097b0ad0be425cb6b7696b89
farhanzipp <farhanaji317@gmail.com>
2024-07-29 16:56:53 +0700

    feat: logout on dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator"
 import { Metadata } from "next"
 import { SidebarNav } from "./sidebar-nav"
+import { LogoutButton } from "./logout-btn"
 
 export const metadata: Metadata = {
   title: "Forms",
@@ -30,11 +31,12 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
       <div className="hidden space-y-6 p-10 pb-16 md:block">
-        <div className="space-y-0.5">
+        <div className="space-y-0.5 flex">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
             Manage your account settings and set e-mail preferences.
           </p>
+          <LogoutButton />
         </div>
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
